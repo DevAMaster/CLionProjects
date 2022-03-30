f6b0ef0ef2c241ada7be21289d9d72d786977cdd
Dev Master <zoomerchan12@gmail.com>
2022-03-30 13:34:52 -0400

    Some Comments

--- TestingMultipleFiles/main.cpp
+++ TestingMultipleFiles/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream> // we need iostream here too since we use it in this file as well
 
+//This is just me utilizing 2 different cpp files, yet if you just use one header files you will be
+//transfering the contents of it for example any variabeles to the main cpp.
+
+
+//#ifndef ADD_H
+//#define ADD_H
+//int add(int x, int y);
+//#endif
+
+//Like java projects, the h file is the blueprint class.
 int getInteger(); // forward declaration for function getInteger
 
 int main()
